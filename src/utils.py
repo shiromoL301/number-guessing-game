8a8590b22db3724038ed7fb605f7e45592f08795
shiromo301 <shiromoto.lab@gmail.com>
2022-10-31 17:38:46 +0900

    Update: add into_speech_bubble() docs

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 def into_speech_bubble(msg: str) -> str:
+    """文字列を吹き出しで囲む
+
+    Args:
+        msg (str): 表示したい文字列
+
+    Returns:
+        str: 吹き出しで囲んだ文字列
+    """
     length = 2 * len(msg)
     buffer = ["＿人"]
     for _ in range(length // 2):
